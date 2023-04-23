df4304e3f0465cb70b4c79f6ff88730ffbcfa1a9
Ritin George <78461388+aurora-0025@users.noreply.github.com>
2023-04-23 18:56:26 +0530

    update: contract to erc721

--- contracts/Stray.sol
+++ contracts/Stray.sol
@@ -2,23 +2,28 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Stray is ERC721A, Ownable {
+contract Stray is ERC721, Ownable {
     uint256 public maxSupply;
     uint256 public tokenPrice = 1 gwei;
+	uint256 public totalMinted = 0;
     string private _baseTokenURI;
 	address payable public withdrawWalletAddress;
 
-    constructor(uint256 max_supply, string memory base_token_URI) ERC721A("Stray", "STRAY") {
+    constructor(uint256 max_supply, string memory base_token_URI) ERC721("Stray", "STRAY") {
 		maxSupply = max_supply;
 		_baseTokenURI = base_token_URI;
 	}
 
     function mint(uint256 quantity) external payable {
-		require(_totalMinted() + quantity <= maxSupply, 'Not enough supply left');
+		require(totalMinted + quantity <= maxSupply, 'Not enough supply left');
 		require(msg.value >= quantity * tokenPrice, 'Insufficent Funds');
-        _mint(msg.sender, quantity);
+
+		for (uint256 i = 0; i < quantity; i++) {
+			totalMinted++;
+			_safeMint(msg.sender, totalMinted);
+		}
     }
 
 
@@ -34,7 +39,7 @@ contract Stray is ERC721A, Ownable {
 	function getOwnerTokens(address owner) public view returns (uint[] memory) {
 		uint[] memory result = new uint[](balanceOf(owner));
 		uint count = 0;
-		for(uint i = 0; i < _totalMinted(); i++) {
+		for(uint i = 0; i < totalMinted; i++) {
 			if(ownerOf(i) == owner) {
 				result[count] = i;
 				count++;
@@ -63,7 +68,7 @@ contract Stray is ERC721A, Ownable {
 	// OVERRIDE FUNCTIONS
 	function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
 		require(_exists(tokenId), 'Token does not exist');
-		return string(abi.encodePacked(_baseTokenURI, _toString(tokenId), ".json"));
+		return string(abi.encodePacked(_baseTokenURI, Strings.toString(tokenId), ".json"));
 	}
 
     function _baseURI() internal view override returns (string memory) {
